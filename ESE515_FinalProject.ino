f5ff47b0d703fc7a0ed80a332ad96b81b37f7a21
adideva98 <56041407+adideva98@users.noreply.github.com>
2023-04-26 16:22:30 -0400

    Updated to use Wifi Rev2 and cleaned code

--- ESE515_FinalProject.ino
+++ ESE515_FinalProject.ino
@@ -1,6 +1,24 @@
-//#include <DFRobot_DHT11.h>
-
+#include <WiFiNINA.h>
+#include<pins_arduino.h>
 #include "dht.h"
+
+#define AIO_USERNAME    "Adideva"
+#define AIO_KEY         "aio_OdOh64Yf6Hm8Sm2dP4VIQDhojdoX"
+#define AIO_TEMP_FEED    "temperature"
+#define AIO_HUMID_FEED  "humidity"
+#define AIO_GAS_FEED    "gas"
+#define AIO_WATER_FEED  "water"
+#define AIO_FIRE_FEED   "fire"
+#define WIFI_SSID       "PhillMax"
+#define WIFI_PASS       "daretodream"
+#define USE_AIRLIFT     // required for Arduino Uno WiFi R2 board compatability
+#include <AdafruitIO_WiFi.h>
+AdafruitIO_WiFi aio(AIO_USERNAME, AIO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS, SPIWIFI_ACK, SPIWIFI_RESET, NINA_GPIO0, &SPI);
+AdafruitIO_Feed *tempFeednew = aio.feed(AIO_TEMP_FEED);
+AdafruitIO_Feed *humidFeednew = aio.feed(AIO_HUMID_FEED);
+AdafruitIO_Feed *gasfeed=aio.feed(AIO_GAS_FEED);
+AdafruitIO_Feed *waterfeed=aio.feed(AIO_WATER_FEED);
+AdafruitIO_Feed *firefeed=aio.feed(AIO_FIRE_FEED);
 #define dht_apin A0 // Analog Pin sensor is connected to Arduino
 dht DHT;
 #define POWER_PIN  7
@@ -8,9 +26,9 @@ dht DHT;
 #define MQ2pin A2
 #define PUMP_PIN A3
 
-float gas_value;
+float gas_value=0.0;
 int water_value = 0;
-int fire_value=0;
+int fire_value=0; 
 int water_level_flag=0;
 float temperature = 0.0;
 float humidity = 0.0;
@@ -18,7 +36,17 @@ bool is_fire=0;
 int smoke_high=0;
 bool watering=0;
 
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
 void setup() {
+    
     Serial.begin(9600);
     delay(20000);
     pinMode(2, INPUT); //initialize Flame sensor output pin connected pin as input.
@@ -26,6 +54,16 @@ void setup() {
     pinMode(POWER_PIN, OUTPUT);   // configure D7 pin as an OUTPUT
     pinMode(PUMP_PIN, OUTPUT);
     digitalWrite(POWER_PIN, LOW);
+    lcd.begin(16, 2);
+    while(!Serial);  
+    Serial.print("Connecting to Adafruit IO");
+    aio.connect(); 
+    while(aio.status() < AIO_CONNECTED) {
+      Serial.print(".");
+      delay(1000);  // wait 1 second between checks
+   }
+    Serial.println();
+    Serial.println(aio.statusText());
     
 
 }
@@ -119,30 +157,61 @@ else {
 return is_fire;
 }
 void loop() {
-
-  digitalWrite(POWER_PIN, HIGH);  // turn the sensor ON
-  delay(10);                      // wait 10 milliseconds
-  water_value = analogRead(SIGNAL_PIN); // read the analog value from sensor
-  digitalWrite(POWER_PIN, LOW);   // turn the sensor OFF
-  //Serial.print("Sensor value: ");
-  //Serial.println(water_value);
-  delay(1000);
-  fire_value=digitalRead(2);
-  // fire_value=0;
-  gas_value = analogRead(MQ2pin);
+  aio.run();
+  lcd.setCursor(0, 0);
   DHT.read11(dht_apin);
-  temperature= DHT.temperature;
-  humidity = DHT.humidity;
-  Serial.println(temperature);
-  Serial.println(gas_value);
-  Serial.println(water_value);
-  Serial.println(humidity);
-  check_smoke();
-  check_fire();
-  check_water();
-  start_water();
-  Serial.println(is_fire);
-  Serial.println(watering);
-  delay(2000);
+  lcd.print("Humidity= ");
+  lcd.print(DHT.humidity);
+  lcd.setCursor(0, 1);
+  lcd.print("Temp= ");
+  lcd.print(DHT.temperature); 
+  delay(1000);
+  lcd.clear();
+  
+  digitalWrite(POWER_PIN, HIGH);  
+  delay(10);                      
+  water_value = analogRead(SIGNAL_PIN); 
+  digitalWrite(POWER_PIN, LOW);   
+  delay(1000);
+  fire_value=digitalRead(6);
+  gas_value = analogRead(MQ2pin);
+  
+  delay(500);
+  lcd.setCursor(0,0);
+  lcd.print("Smoke Val= ");  
+  lcd.print(gas_value);
+  lcd.setCursor(0,1);
+  lcd.print("Water Val= ");     
+  lcd.print(water_value);
+  delay(1000); 
+  lcd.clear();   
 
+  check_smoke();
+  
+  check_fire();
+  
+  check_water();
+  
+  start_water();
+
+  delay(500);
+  lcd.setCursor(0,0);
+  if (is_fire==1)
+  {
+    lcd.print("Fire! Fire! ");    
+  }
+  else
+  {
+    lcd.print("No fire. Safe!");
+  }
+
+  delay(1000); 
+  lcd.clear();
+  tempFeednew->save(temperature);  // send potentiometer value to AIO
+  humidFeednew->save(humidity);  // send potentiometer value to AIO
+  gasfeed->save(gas_value);
+  waterfeed->save(water_value);
+  firefeed->save(is_fire);
+  delay(5000);   
+  
 }
